d0736d277ac42b10324bd2d338985932b29bd8ac
Bicheng Yang <superybc@outlook.com>
2021-07-27 23:05:24 +0800

    build opensbi in rv64g without payload

--- scripts/build_opensbi.sh
+++ scripts/build_opensbi.sh
@@ -6,5 +6,6 @@
 
 cd /home/penglai/penglai-enclave/opensbi-0.9
 mkdir -p build-oe/qemu-virt
-CROSS_COMPILE=riscv64-unknown-linux-gnu- make O=build-oe/qemu-virt PLATFORM=generic FW_PAYLOAD=y FW_PAYLOAD_PATH=/home/penglai/penglai-enclave/Image
+#CROSS_COMPILE=riscv64-unknown-linux-gnu- make O=build-oe/qemu-virt PLATFORM=generic FW_PAYLOAD=y FW_PAYLOAD_PATH=/home/penglai/penglai-enclave/openEuler-files/Image
+CROSS_COMPILE=riscv64-unknown-linux-gnu- make O=build-oe/qemu-virt PLATFORM=generic FW_PAYLOAD=n
 #cp build-oe/qemu-virt/platform/qemu/virt/firmware/fw_payload.elf build-oe/qemu-virt/boot/fw_payload_oe_qemuvirt.elf
